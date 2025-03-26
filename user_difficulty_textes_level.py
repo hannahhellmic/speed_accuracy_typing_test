3c93190e35c21f863d4f0c08708f6a9341836cc5
hannahhellmic <167358202+hannahhellmic@users.noreply.github.com>
2025-03-27 01:12:44 +0300

    Create user_difficulty_textes_level.py

---
+++ user_difficulty_textes_level.py
@@ -0,0 +1,72 @@
+level1 = [("I live in a house near the mountains. "
+            "I have two brothers and one sister, and I was born last. "
+            "My father teaches mathematics, and my mother is a nurse at a big hospital. "
+            "My brothers are very smart and work hard in school. "
+            "My sister is a nervous girl, but she is very kind. "
+            "My grandmother also lives with us. "
+            "She came from Italy when I was two years old. "
+            "She has grown old, but she is still very strong. "
+            "She cooks the best food! "),
+          ("Please note that passengers can only take ONE suitcase onto the plane. "
+           "It must be no bigger than 55cm x 22cm x 35cm and weigh no more than 10kg. "
+           "You can also take one small laptop bag or handbag that can fit under the seat in front of you. "
+           "If you have two bags, their total weight cannot be more than 10kg. "
+           "If your bag is too big or too heavy, you will not be allowed to take it onto the plane."),
+          ("How are you? " 
+           "It was so nice to meet you last week in Sydney at the sales meeting. "
+           "How was the rest of your trip? "
+           "Did you see any kangaroos? "
+           "I hope you got home to Mexico City OK."
+           "Anyway, I have the documents about the new Berlin offices. "
+           "We are going to be open in three months. "
+           "I moved here from London just last week."),
+          ("I am an experienced sales manager with 12 years experience in service teams. "
+           "I am skilled in team motivation and building successful sales teams. "
+           "After ten years of working in sales and customer service at Halo Bank. " 
+           "I am now the sales office manager of a team of 120 at Southern General Plastics Ltd."
+           "I have an MBA from Stanford University and a degree in Business Studies from Cornell University.")
+]
+
+level2 = [("The pandemic hit during World War I and devastated military troops. "
+        "In the United States, for instance, more servicemen were killed from the flu than from the war itself. "
+        "The Spanish flu was fatal to a higher proportion of young adults than most flu viruses. "
+        "The pandemic started mildly, in the spring of 1918, but was followed by a much more severe wave in the fall of 1918. "
+        "The war likely contributed to the devastating mortality numbers, as large outbreaks occurred in military forces living in close quarters."),
+          ("Whether you're travelling to the islands or the mountains of Thailand, you're likely to spend at least one night in its capital city on the way. "
+           "Bangkok might be noisy and polluted but it is also an exciting city with plenty of things to see and do. Why not make it a longer stay? "
+           "The Khao San Road was a famous traveller spot even before the caracher of Leonardo di Caprio in the film The Beach stayed there. "
+           "But it is noisy, not very pretty and not very Thai. " 
+           "Bangkok's traffic can be a nightmare."),
+          ("Every time you are online, you are bombarded by pictures, articles, links and videos trying to tell their story. "
+           "Unfortunately, not all of these stories are true. "
+           "Sometimes they want you to click on another story or advertisement at their own site, other times they want to upset people for political reasons. "
+           "These days it's so easy to share information. "
+           "These stories circulate quickly, and the result is … fake news."),
+          ("If you think of the jobs robots could never do, you would probably put doctors and teachers at the top of the list. "
+           "It's easy to imagine robot cleaners and factory workers, but some jobs need human connection and creativity. "
+           "But are we underestimating what robots can do? "
+           "In some cases, they already perform better than doctors at diagnosing illness. "
+           "Also, some patients might feel more comfortable sharing personal information with a machine than a person. "
+           "Could there be a place for robots in education after all?")
+]
+
+level3 = [("Chicago has many historic places to visit. "
+           "Keith found the Chicago Water Tower impressive as it is one of the few remaining landmarks to have survived the Great Chicago Fire of 1871. "
+           "Keith also took a walk through Jackson Park, a great outdoor space that hosted the World's Fair of 1892. "
+           "The park is great for a leisurely stroll, and it still features some of the original architecture and replicas of monuments that were featured in the World's Fair."),
+          ("This stark difference in opinion over something that could be conceived as minor and thus easily overlooked goes to show that we often attach meaning to even the most mundane practices. "
+           "When things that we are used to are done differently, it could spark the strongest reactions in us. "
+           "The best thing to do is to listen and observe how your conversation partner addresses you and, if you are still unsure, do not be afraid to ask them how they would like to be addressed."),
+          ("For more than two hundred years the pessimists have been winning the public debate. "
+           "They tell us that things are getting worse. "
+           "But in fact, life is getting better. "
+           "Income, food availability and lifespan are rising; disease, violence and child mortality are falling. "
+           "These trends are happening all around the world. "
+           "Africa is slowly coming out of poverty, just as Asia did before. "
+           "The internet, mobile phones and worldwide trade are making the lives of millions of people much better."),
+          ("We know that human language is far more complex than that of even our nearest and most intelligent relatives like chimpanzees. "
+           "We can express complex thoughts, convey subtle emotions and communicate about abstract concepts such as past and future. "
+           "And we do this following a set of structural rules, known as grammar. "
+           "Do only humans use an innate system of rules to govern the order of words? "
+           "Perhaps not, as some research may suggest dolphins share this capability because they are able to recognise when these rules are broken.")
+]
